935c5fcd9c591b0d800a2015929a13b464402f89
wjpas <wjpasq@gmail.com>
2019-08-17 11:56:42 -0500

    Added Main Activity Welcome message and map activity.

--- app/src/main/java/com/example/bikespot/MainActivity.java
+++ app/src/main/java/com/example/bikespot/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.bikespot;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button_findBikeRack;
+    private TextView text_welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_findBikeRack = findViewById(R.id.button_findBikeRack);
+        text_welcome = findViewById(R.id.textView_welcome);
+
+        button_findBikeRack.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent mapIntent = new Intent(this, MapsActivity.class);
+        startActivity(mapIntent);
     }
 }
