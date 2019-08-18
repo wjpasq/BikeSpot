e210b7b08b95c582024338ea8512bd06e1665c5b
Landon Palmer <landonjpalmer@gmail.com>
2019-08-18 09:39:39 -0500

    Added more elements to fragment

--- app/src/main/java/com/example/bikespot/BottomSheetDialog.java
+++ app/src/main/java/com/example/bikespot/BottomSheetDialog.java
@@ -14,22 +14,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class BottomSheetDialog extends BottomSheetDialogFragment {
-        private BikePlace bikePlace;
-        private TextView businessName;
-        private TextView address;
+
+    private BikePlace bikePlace;
+    private TextView businessName;
+    private TextView address;
+    private TextView openSpots;
+    private TextView totalSpots;
+    private TextView parkingType;
 
 
-        public BottomSheetDialog(BikePlace bikePlace, AppCompatActivity activity) {
-            super();
-            this.bikePlace = bikePlace;
-        }
-
-        private void formatText() {
-
-            businessName.setText(bikePlace.getBusinessName());
-            address.setText(bikePlace.getAddress());
-
-        }
+    public BottomSheetDialog(BikePlace bikePlace, AppCompatActivity activity) {
+        super();
+        this.bikePlace = bikePlace;
+    }
 
 
     @Nullable
@@ -38,7 +35,20 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
         View v = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
         businessName = v.findViewById(R.id.textView_businessName);
         address = v.findViewById(R.id.textView_address);
+        totalSpots = v.findViewById(R.id.textView_totalSpaces);
+        openSpots = v.findViewById(R.id.textView_openSpaces);
+        parkingType = v.findViewById(R.id.textView_parkingType);
         formatText();
         return v;
+    }
+
+    private void formatText() {
+
+        businessName.setText(bikePlace.getBusinessName());
+        address.setText(bikePlace.getAddress());
+        totalSpots.setText("Total Spaces: " + bikePlace.getTotalSpaces());
+        openSpots.setText("Open Spaces: " + bikePlace.getOpenSpaces());
+        parkingType.setText("Parking Type: " + bikePlace.getParkingModules());
+
     }
 }
